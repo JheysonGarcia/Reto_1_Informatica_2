94f0b56d9280b562826f5521f3e611f8ee7d0246
JheysonGarcia <jheyson.garcia@udea.edu.co>
2024-09-12 01:05:19 +0000

    - Se introduce la copia del ejercicio entregado. - Se inicia trabajo con circuitos y conexiones.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int analogPin = 0;
+int val = 0;
+
+void setup(){
+    Serial.begin(9600);
+
+}
+
+void loop(){
+    val = analogRead(analogPin);
+    Serial.printIn(val);
+    
+}   
